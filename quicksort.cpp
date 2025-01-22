76fce458763da51c40a14beeeb237b51847c1b4a
Ananya Singh <singhananya5040@gmail.com>
2025-01-22 16:30:12 +0530

    LPU DSA

---
+++ quicksort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int partition(int arr[], int start, int end)
+{
+    int smallCount = 0;
+    int pivot = arr[start];
+    for (int i = start+1 ; i <= end; i++)
+    {
+        //counting smaller element:
+        if (arr[i] <= pivot) //check why did we add equal to.
+            smallCount++;
+    }
+    //placing pivot at the right position:
+    int pivotIndex = start + smallCount;
+    swap(arr[pivotIndex],arr[start]);
+
+    //left-right wala part:
+    int i= start;
+    int j= end;
+    while(i<pivotIndex && j>pivotIndex)
+    {
+        while(arr[i]<=pivot)
+        {
+            i++;
+        }
+        while(arr[j]>pivot) //duplicates counting.
+        {
+            j--;
+        }
+        if(i<pivotIndex && j>pivotIndex)
+        {
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+void quickSort(int arr[], int start, int end)
+{
+    //Base Case:
+    if(start>=end)
+    return;
+    //partition karo:
+    int p= partition(arr,start,end);
+    //left side sorting:
+    quickSort(arr,start,p-1);
+    //right side sorting:
+    quickSort(arr,p+1,end);
+}
+int main()
+{
+    int arr[6]= {5,18,32,60,2,15};
+    quickSort(arr,0,5);
+    cout<<"Sorted Array: ";
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i]<<" , ";
+    }
+    return 0;
+}
---
+++ singly_linkedlist.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+class LinkedListNode
+{
+
+    // LinkedListNode class datatype ka pointer to store address of next node.
+public:
+    int data;
+    LinkedListNode *next;
+
+    // Constructor to assign values.
+    LinkedListNode(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+void insertAtHead(LinkedListNode *&head, int data)
+{
+    // creation of new node.
+    LinkedListNode *temp = new LinkedListNode(data);
+    temp->next = head;
+    head = temp;
+}
+// void insertAtTail(LinkedListNode *&head, int data)
+// {
+//     LinkedListNode *new_node = new LinkedListNode(data);
+//     LinkedListNode *temp = head;
+//     while(temp->next!=NULL)
+//     {
+//         temp= temp->next;
+//     }
+//     temp->next = new_node;
+
+// }
+void insertAtTail(LinkedListNode *&tail, int data)
+{
+     LinkedListNode *temp = new LinkedListNode(data);
+     tail->next= temp;
+     tail= tail->next;
+}
+void insertAtPosition(LinkedListNode *&head, LinkedListNode *&tail,  int position, int data)
+{
+    //starting insertion:
+    if(position==1)
+    {
+        insertAtHead(head,data);
+        return;
+    }
+    
+   LinkedListNode* temp= head;
+   int count = 1; //that is abhi head pe hai.
+   while(count < position-1)
+   {
+     temp= temp->next;
+     count++;
+   }
+   if(temp->next==NULL)
+   {
+    insertAtTail(tail,data);
+    return;
+   }
+   else{
+    LinkedListNode *nodeToInsert = new LinkedListNode(data);
+    nodeToInsert->next= temp->next;
+    temp->next= nodeToInsert;
+   }
+}
+
+void FindMiddleElement(LinkedListNode*&head)
+{
+    //BRUTE FORCE:
+   int count =1;
+    LinkedListNode* temp = head;
+    while(temp->next!=NULL)
+    {
+        temp= temp->next;
+        count ++;
+    }
+    int mid= (count/2)  ;
+    cout<<"mid: "<<mid<<" "<<"count: "<<count<<endl;
+    LinkedListNode* temp1= head;
+    // while(mid>1)
+    // {
+    //     temp1= temp1->next;
+    //     mid--;
+
+    // }
+    for(int i=mid-1;i>0;i--)
+    temp1= temp1->next;
+    cout<<temp1->data<<endl;
+}
+
+void printList(LinkedListNode *&head)
+{
+
+    LinkedListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << " NULL" << endl;
+}
+
+int main()
+{
+
+    LinkedListNode *node1 = new LinkedListNode(10); // object banaya.
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    // head pointing to new node.
+    LinkedListNode *head = node1;
+    LinkedListNode *tail = node1;
+    insertAtHead(head, 12);
+    insertAtHead(head, 14);
+    insertAtTail(tail, 15);
+    insertAtTail(tail, 18);
+    insertAtTail(tail, 39);
+    insertAtTail(tail, 45);
+    insertAtTail(tail, 22);
+    insertAtPosition(head,tail, 3,8);
+    printList(head);
+    FindMiddleElement(head);
+}
