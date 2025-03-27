7d9c01062d88c4c0b0f29ece7c818f5e4f14ca95
Ananya Singh <singhananya5040@gmail.com>
2025-03-27 14:54:28 +0530

    27-03-25

---
+++ longestchar.cpp
@@ -0,0 +1,12 @@
+// //Longest repetetive character in a string
+// int maxi =0;
+// unordered_map<char,int> mpp;
+// for(char ch: s)
+// {
+//     mpp[ch]++;
+// }
+// for(auto it: mpp)
+// {
+//     maxi = max(maxi, it.second);
+// }
+// return maxi;
